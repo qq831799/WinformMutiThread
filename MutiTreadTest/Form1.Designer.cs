e5d1ab4c7802b6cc1d3540aa32c41469091ff2a0
qq831799@gmail.com <33570410+qq831799@users.noreply.github.com>
2019-04-28 21:49:23 +0800

    initial

---
+++ MutiTreadTest/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace MutiTreadTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.resultGrid = new System.Windows.Forms.DataGridView();
+            this.folderName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.threadName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.resultGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(277, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(274, 123);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(41, 14);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "label1";
+            // 
+            // resultGrid
+            // 
+            this.resultGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.resultGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.resultGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.folderName,
+            this.threadName});
+            this.resultGrid.Location = new System.Drawing.Point(434, 123);
+            this.resultGrid.Name = "resultGrid";
+            this.resultGrid.RowTemplate.Height = 26;
+            this.resultGrid.Size = new System.Drawing.Size(304, 150);
+            this.resultGrid.TabIndex = 2;
+            // 
+            // folderName
+            // 
+            this.folderName.HeaderText = "Folder Name";
+            this.folderName.Name = "folderName";
+            // 
+            // threadName
+            // 
+            this.threadName.HeaderText = "Thread Name";
+            this.threadName.Name = "threadName";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(277, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.resultGrid);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.resultGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.DataGridView resultGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn folderName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn threadName;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MutiTreadTest/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MutiTreadTest
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int add(int x, int y) => x + y;
+            Dictionary<string, string[]> test(Object a) => new Dictionary<string, string[]>();
+            Func<Object, Dictionary<string, string[]>> fileProcess = ((folder) =>
+            {
+                var folderInfo = (KeyValuePair<string, string[]>)folder;
+
+                List<Task<string>> childTasks = new List<Task<string>>();
+                List<string> fileArray = new List<string>();
+                foreach (var file in folderInfo.Value)
+                {
+                    childTasks.Add(Task<string>.Factory.StartNew((name) =>
+                    {
+                        Random random = new Random(Guid.NewGuid().GetHashCode());
+                        int sleepTime = random.Next(1, 5);
+                        Console.WriteLine($"{folderInfo.Key} - {name} Sleep {sleepTime} Sec");
+                        Thread.Sleep(sleepTime * 1000);
+                        Console.WriteLine($"{folderInfo.Key} - {name} wake up");
+                        return $"{name} file";
+                    }, file));
+                }
+                return new Dictionary<string, string[]>() { { folderInfo.Key, childTasks.Select(task => task.Result).ToArray() } };
+            });
+
+            statusLabel.Text = "處理中";
+            Dictionary<string, string[]> folderAndFiles = new Dictionary<string, string[]>()
+            {
+                {"Folder A",new string[] { "AA thread", "AB thread", "AC thread" } },
+                {"Folder B",new string[] { "BA thread", "BB thread"} },
+                {"Folder C",new string[] { "CA thread", "CB thread", "CC thread" } },
+            };
+            List<Task<Dictionary<string, string[]>>> parentTasks = new List<Task<Dictionary<string, string[]>>>();
+            foreach (var folder in folderAndFiles)
+            {
+                parentTasks.Add(Task<Dictionary<string, string[]>>.Factory.StartNew(fileProcess, folder));               
+
+            }
+            parentTasks.ForEach(task => task.ContinueWith(t =>
+            {
+                foreach (var result in t.Result)
+                {
+                    foreach (var file in result.Value)
+                    {
+                        resultGrid.Rows.Add(result.Key, file);
+                    }
+                }
+                
+            }, TaskScheduler.FromCurrentSynchronizationContext()));
+
+            Task.WhenAll(parentTasks.ToArray()).ContinueWith((t)=> statusLabel.Text = "完成", TaskScheduler.FromCurrentSynchronizationContext());
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            resultGrid.Rows.Clear();
+        }
+    }
+}
